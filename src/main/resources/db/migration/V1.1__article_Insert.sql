8f7ca5212f31e4f4b99be5d07b30a21b4ccb4fc2
MuhammadNurYanhaona <mny9md@virginia.edu>
2017-08-15 11:43:41 +0600

    Added flyway migration support to the project

---
+++ src/main/resources/db/migration/V1.1__article_Insert.sql
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  yan
+ * Created: Aug 15, 2017
+ */
+
+INSERT INTO articles (article_id, title, category) 
+VALUES
+    (1, 'Java Concurrency', 'Java'),
+    (2, 'Hibernate HQL ', 'Hibernate'),
+    (3, 'Spring MVC with Hibernate', 'Spring');
+
