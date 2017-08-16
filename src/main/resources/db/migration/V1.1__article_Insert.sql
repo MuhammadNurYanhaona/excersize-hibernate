a2ef410cf3f6e391c09caccbc7d8a8fb76e7b89a
Muhammad Nur <Muhammad Nur>
2017-08-16 14:04:12 +0600

    Added oracle properties in the project

--- src/main/resources/db/migration/V1.1__article_Insert.sql
+++ src/main/resources/db/migration/V1.1__article_Insert.sql
@@ -8,9 +8,8 @@
  * Created: Aug 15, 2017
  */
 
-INSERT INTO articles (article_id, title, category) 
-VALUES
-    (1, 'Java Concurrency', 'Java'),
-    (2, 'Hibernate HQL ', 'Hibernate'),
-    (3, 'Spring MVC with Hibernate', 'Spring');
+INSERT INTO articles (article_id, title, category) VALUES (1, 'Java Concurrency', 'Java');
+INSERT INTO articles (article_id, title, category) VALUES (2, 'Hibernate HQL ', 'Hibernate');
+INSERT INTO articles (article_id, title, category) VALUES (3, 'Spring MVC with Hibernate', 'Spring');
+
 
